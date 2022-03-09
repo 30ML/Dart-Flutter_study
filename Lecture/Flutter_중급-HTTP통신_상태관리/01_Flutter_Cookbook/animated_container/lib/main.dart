f2bf965b0364487fe4d04639d62438bd106303c6
Minjae Jay Kim <exploit017@gmail.com>
2022-03-09 18:54:46 +0900

    Animated Container 학습

---
+++ Lecture/Flutter_중급-HTTP통신_상태관리/01_Flutter_Cookbook/animated_container/lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated Container',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadius _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animated Container'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          duration: const Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+          // color: _color,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+            _color = Color.fromRGBO(
+              random.nextInt(256),
+              random.nextInt(256),
+              random.nextInt(256),
+              1,
+            );
+            _borderRadius =
+                BorderRadius.circular(random.nextInt(100).toDouble());
+          });
+        },
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
