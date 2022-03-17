800e26b0775b1bfde0d45cdf1fdf978acf1148f9
Minjae Jay Kim <exploit017@gmail.com>
2022-03-17 16:44:51 +0900

    API 테스트 코드 작성

--- Lecture/Flutter_중급-HTTP통신_상태관리/air_pollution_app/test/widget_test.dart
+++ Lecture/Flutter_중급-HTTP통신_상태관리/air_pollution_app/test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:air_pollution_app/main.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  test('http 통신 test', () async {
+    Uri url = Uri.parse(
+        'http://api.airvisual.com/v2/nearest_city?key=5bd2e943-8112-49f8-849b-8fed1e034095');
+    var response = await http.get(url);
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    print(response.body);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(response.statusCode, 200);
   });
 }
