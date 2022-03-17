d45960dd35fac99ed87af0f446c6cc24decf3d9e
Minjae Jay Kim <exploit017@gmail.com>
2022-03-17 18:11:02 +0900

    counter 앱 - setState()를 사용한 데이터 업데이트

---
+++ Lecture/Flutter_중급-HTTP통신_상태관리/counter_app/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Counter(),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  const Counter({Key? key}) : super(key: key);
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              child: Text('Add'),
+              color: Colors.grey[400],
+              onPressed: () {
+                setState(() {
+                  _count += 1;
+                });
+              },
+            ),
+            const SizedBox(height: 8),
+            Text(
+              '$_count',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
