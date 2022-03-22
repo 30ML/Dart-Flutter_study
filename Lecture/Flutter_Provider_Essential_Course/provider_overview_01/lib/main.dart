b72f67765ea8984bf19e4d3d9e1e52a749e1b7b1
Min Jae Kim <exploit017@gmail.com>
2022-03-22 17:12:35 +0900

    기본 코드 세팅

--- Lecture/Flutter_Provider_Essential_Course/provider_overview_01/lib/main.dart
+++ Lecture/Flutter_Provider_Essential_Course/provider_overview_01/lib/main.dart
@@ -27,8 +27,119 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter - Provider Overview 01'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: Colors.blue[100],
+              padding: const EdgeInsets.all(20),
+              child: const Text(
+                'MyHomePage',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            const SizedBox(height: 20),
+            CounterA(),
+            const SizedBox(height: 20),
+            Middle(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CounterA extends StatelessWidget {
+  const CounterA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red[100],
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text(
+            '0',
+            style: const TextStyle(fontSize: 48),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text(
+              'Increment',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Middle extends StatelessWidget {
+  const Middle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CounterB(),
+          const SizedBox(width: 20),
+          Sibling(),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterB extends StatelessWidget {
+  const CounterB({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow[100],
+      padding: const EdgeInsets.all(10),
+      child: Text(
+        '0',
+        style: const TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class Sibling extends StatelessWidget {
+  const Sibling({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange[100],
+      padding: const EdgeInsets.all(10),
+      child: const Text(
+        'Sibling',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
   }
 }
