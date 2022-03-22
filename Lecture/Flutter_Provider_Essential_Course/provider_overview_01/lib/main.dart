5db5730ddcdf9a37b20b5f4ffc929ae72aa97f32
Min Jae Kim <exploit017@gmail.com>
2022-03-22 16:56:10 +0900

    앱 이름 수정

--- Lecture/Flutter_Provider_Essential_Course/provider_overview_01/lib/main.dart
+++ Lecture/Flutter_Provider_Essential_Course/provider_overview_01/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Provider Overview 01',
+      title: 'Counter - Provider Overview 01',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
