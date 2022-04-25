e06fa0a7fedabbd71c7208a3617a96b40226cd70
Min Jae Kim <exploit017@gmail.com>
2022-04-25 14:33:16 +0900

    edit directory

---
+++ Flutter_Clean_Architecture/image_search_app_03_viewmodel_test/android/app/src/main/kotlin/com/example/image_search_app_03_viewmodel_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_search_app_03_viewmodel_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter_Clean_Architecture/image_search_app_03_viewmodel_test/lib/ui/home_view_model.dart
@@ -0,0 +1,22 @@
+// PixabayApi를 가지고 무언가는 하는 파일
+
+import 'dart:async';
+
+import 'package:image_search_app_03_viewmodel_test/data/api.dart';
+import 'package:image_search_app_03_viewmodel_test/model/photo.dart';
+
+class HomeViewModel {
+  final PixabayApi api;
+
+  final _photoStreamController = StreamController<List<Photo>>()..add([]);
+  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
+
+  HomeViewModel(
+    this.api,
+  );
+
+  void fetch(String query) async {
+    final result = await api.fetchPhoto(query);
+    _photoStreamController.add(result);
+  }
+}
