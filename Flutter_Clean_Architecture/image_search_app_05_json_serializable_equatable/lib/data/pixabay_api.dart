4c57f25359da58a2b8e52189b5dbfcaf32152121
Min Jae Kim <exploit017@gmail.com>
2022-04-25 16:39:48 +0900

    init project(json_serializable, equatable)

---
+++ Flutter_Clean_Architecture/image_search_app_05_json_serializable_equatable/lib/data/pixabay_api.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:image_search_app_05_json_serializable_equatable/data/photo_api_repository.dart';
+import 'package:image_search_app_05_json_serializable_equatable/model/photo.dart';
+import 'package:http/http.dart' as http;
+
+class PixabayApi implements PhotoApiRepository {
+  static const baseURL = 'https://pixabay.com/api/';
+  static const key = '24829672-e86462931ca2f2cf80fb27137';
+
+  @override
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    // if (client == null) {
+    //   client = http.Client();
+    // }
+    client ??= http.Client();
+
+    // final response = await http.get(
+    final response = await client.get(
+      Uri.parse('$baseURL?key=$key&q=$query&image_type=photo'),
+    );
+    Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+    Iterable hits = jsonResponse['hits'];
+
+    return hits.map((e) => Photo.fromMap(e)).toList();
+  }
+}
