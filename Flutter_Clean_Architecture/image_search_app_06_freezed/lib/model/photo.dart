e4d62d4842735e47f8e140543a9498742fe968c2
hororoc <exploit017@gmail.com>
2022-04-26 14:43:30 +0900

    freezed generate

--- Flutter_Clean_Architecture/image_search_app_06_freezed/lib/model/photo.dart
+++ Flutter_Clean_Architecture/image_search_app_06_freezed/lib/model/photo.dart
@@ -1,40 +1,15 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-import 'package:equatable/equatable.dart';
+part 'photo.freezed.dart';
+part 'photo.g.dart';
 
-class Photo extends Equatable {
-  final int id;
-  final String tags;
-  final String previewURL;
+@freezed
+class Photo with _$Photo {
+  factory Photo({
+    required int id,
+    required String tags,
+    required String previewURL,
+  }) = _Photo;
 
-  const Photo({
-    required this.id,
-    required this.tags,
-    required this.previewURL,
-  });
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'tags': tags,
-      'previewURL': previewURL,
-    };
-  }
-
-  factory Photo.fromMap(Map<String, dynamic> map) {
-    return Photo(
-      id: map['id'] as int,
-      tags: map['tags'] as String,
-      previewURL: map['previewURL'] as String,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Photo.fromJson(String source) =>
-      Photo.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  List<Object> get props => [id, tags, previewURL];
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
 }
